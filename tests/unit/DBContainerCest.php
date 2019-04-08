e820ddfc9e84936da9d1e6d03720d6444e394545
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-04-08 12:58:43 +0530

    Increased the thread sleep to 60sec

--- tests/unit/DBContainerCest.php
+++ tests/unit/DBContainerCest.php
@@ -11,7 +11,7 @@ class DBContainerCest
     }
 
     public function checkContainerIsRunning(UnitTester $I){
-        sleep(30);
+        sleep(60);
         $I->wantTo("verify MySQL 5.5 container is up and running");
         $I->runShellCommand("docker inspect -f {{.State.Running}} dev_mysql");
         $I->seeInShellOutput("true");
